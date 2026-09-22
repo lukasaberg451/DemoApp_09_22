edb05483e6fc99185b9cf7c9807e3bb27baa2cd9
Tronje1 <tronje.skoglund1@gmail.com>
2026-09-22 14:31:32 +0200

    metod för tärnings kast

---
+++ DemoApp_09_22/Tarning.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System;
+
+namespace DemoApp_09_22
+{
+    internal class Tarning
+    {
+        static class Tarning
+        {
+            private Random random = new Random();
+
+            public int Kasta()
+            {
+                return random.Next(1, 7);
+            }
+        }
+    }
+}
